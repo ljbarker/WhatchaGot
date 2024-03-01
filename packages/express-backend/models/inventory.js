912b6b95669beca628be99c1b88205018aa791ad
eswenke <swenke.ethan.us@gmail.com>
2024-02-29 21:26:34 -0800

    added inventory backend files and setup

---
+++ packages/express-backend/models/inventory.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const InventorySchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    item: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    quantity: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    expiration: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { collection: "inventory_list" }
+);
+
+const Inventory = mongoose.model("Inventory", InventorySchema);
+
+export default Inventory;
