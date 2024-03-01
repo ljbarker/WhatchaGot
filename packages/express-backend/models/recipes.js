62536e3b61f34179b5ee40dd0b58f996c9300ea2
ljbarker <loganjb21@outlook.com>
2024-02-29 21:33:26 -0800

    Fully functional multiple ingredient recipe add. Eliminated units combobox to reduce complexity. If needed we can parse input for units.

--- packages/express-backend/models/recipes.js
+++ packages/express-backend/models/recipes.js
@@ -18,8 +18,8 @@ const RecipeSchema = new mongoose.Schema(
     trim: true,
     validate(value) {
         value.forEach(element => {
-            if (element.length < 3)
-                throw new Error("Invalid Ingredient: Must have name, amount, and units.");
+            if (element.length < 2)
+                throw new Error("Invalid Ingredient: Must have name and amount.");
         });
     },
     },
