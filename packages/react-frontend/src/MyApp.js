a6e4f81919ac68a2a015efcf7c1d7e4789b59f4a
Alexander Nunn <adnunn19@gmail.com>
2024-03-05 08:20:06 -0800

    Fixing merge conflicts #1

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -2,7 +2,7 @@ import React, { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import MyRecipes from "./pages/MyRecipes";
 import MyInventory from './pages/MyInventory';
-import MyList from './pages/MyList';
+import MyList from './pages/MyShoppingList';
 import Login from './pages/Login';
 
 function MyApp() {
@@ -13,7 +13,7 @@ function MyApp() {
                 <Route path="/" element={<Home />} />
                 <Route path="/myrecipes" element={<MyRecipes />} />
                 <Route path="/myinventory" element={<MyInventory />} />
-                <Route path="/mylist" element={<MyList />} />
+                <Route path="/myshoppinglist" element={<MyList />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
