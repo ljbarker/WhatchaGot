ae1acbae02968ce2dea0d78877cc9cde5960e2cc
bkennedy06 <bkennedyeble@gmail.com>
2024-03-11 23:21:58 -0700

    RecipeAPI DB struct added, getInv by ingredient function added to inventory-services.js

---
+++ packages/express-backend/models/recipeAPI.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const RecipeAPISchema = new mongoose.Schema({
+  idMeal: { // meal ID
+    type: String,
+    required: true,
+    trim: true,
+  },
+  strMeal: { // Name of meal
+    type: String,
+    required: true,
+    trim: true,
+  },
+  strIngredients: [{ // ingredient list
+    type: String,
+    required: true,
+    trim: true,
+  }],
+  strSource: { // Link to recipe website
+    type: String,
+    required: true,
+    trim: true,
+  },
+  
+}, { collection: "recipe_API" });
+
+const RecipeAPI = mongoose.model("RecipeAPI", RecipeAPISchema);
+
+export default RecipeAPI;
